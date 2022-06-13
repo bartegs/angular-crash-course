efe6ea53faa2fd455978af54a375afe4aefb4435
Bartlomiej Sadecki <barteksadecki@gmail.com>
2022-06-13 10:47:23 +0200

    Angular crash course initial commit + button and header

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent implements OnInit {
+  @Input() text: string;
+  @Input() color: string;
+  @Output() btnClick = new EventEmitter();
+
+  constructor() {
+    this.text = 'click';
+    this.color = 'black';
+  }
+
+  ngOnInit(): void {}
+
+  onClick() {
+    this.btnClick.emit();
+  }
+}
