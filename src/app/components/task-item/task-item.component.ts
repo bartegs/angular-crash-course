ebb97dc47538473a61b93606ad5dcc3ff56e5173
Bartlomiej Sadecki <barteksadecki@gmail.com>
2022-06-13 12:54:13 +0200

    Add task-item and tasks (mock data)

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.scss'],
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task;
+  faTimes = faTimes;
+  
+  constructor() {
+    this.task = {
+      text: '',
+      day: '',
+      reminder: false,
+    };
+  }
+
+  ngOnInit(): void {}
+}
